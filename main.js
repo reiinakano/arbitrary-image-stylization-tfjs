8e9bb8efea67b9b36ac893f76978c4c0ffcf97e2
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2018-11-14 02:43:47 +0900

    tidy calls

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import * as tf from '@tensorflow/tfjs';
 
 /**
@@ -8,11 +9,16 @@ class Main {
     // Initialize images
     this.contentImg = document.getElementById('content-img');
     this.styleImg = document.getElementById('style-img');
-    this.styled100 = document.getElementById('styled-100');
+    this.stylized = document.getElementById('stylized');
 
     // Initialize buttons
     this.styleButton = document.getElementById('style-button');
-    this.styleButton.onclick = () => this.startStyling();
+    this.styleButton.onclick = () => {
+      this.styleButton.disabled = true;
+      this.startStyling().then(() => {
+        this.styleButton.disabled = false;
+      });
+    };
 
     Promise.all([
       tf.loadFrozenModel(
@@ -23,11 +29,11 @@ class Main {
         'saved_model_transformer_js/weights_manifest.json'),
     ]).then(([styleNet, transformNet]) => {
       // Warmup the model. This isn't necessary, but makes the first prediction
-      // faster. Call `dispose` to release the WebGL memory allocated for the return
-      // value of `predict`.
-      const bottleneck = styleNet.predict(tf.zeros([1, 255, 255, 3]));
-      transformNet.predict([tf.zeros([1, 255, 255, 3]), bottleneck]).dispose();
-      bottleneck.dispose();
+      // faster.
+      tf.tidy(() => {
+        const bottleneck = styleNet.predict(tf.zeros([1, 255, 255, 3]));
+        transformNet.predict([tf.zeros([1, 255, 255, 3]), bottleneck]);
+      })
       console.log('Loaded styleNet');  
       this.styleNet = styleNet;
       this.transformNet = transformNet;
@@ -37,14 +43,25 @@ class Main {
 
   netsLoaded() {
     this.styleButton.disabled = false;
+    this.styleButton.textContent = 'Stylize';
   }
 
-  startStyling() {
-    this.styleButton.disabled = true;
-    const bottleneck = this.styleNet.predict(tf.fromPixels(this.styleImg).toFloat().div(tf.scalar(255)).expandDims());
-    const stylized = this.transformNet.predict([tf.fromPixels(this.contentImg).toFloat().div(tf.scalar(255)).expandDims(), bottleneck]);
-    tf.toPixels(stylized.squeeze(), this.styled100);
-    this.styleButton.disabled = false;
+  async startStyling() {
+    await tf.nextFrame();
+    this.styleButton.textContent = 'Generating 100D style representation';
+    await tf.nextFrame();
+    const bottleneck = await tf.tidy(() => {
+      return this.styleNet.predict(tf.fromPixels(this.styleImg).toFloat().div(tf.scalar(255)).expandDims());
+    })
+    this.styleButton.textContent = 'Stylizing image...';
+    await tf.nextFrame();
+    const stylized = await tf.tidy(() => {
+      return this.transformNet.predict([tf.fromPixels(this.contentImg).toFloat().div(tf.scalar(255)).expandDims(), bottleneck]).squeeze();
+    })
+    await tf.toPixels(stylized, this.stylized);
+    bottleneck.dispose();  // Might wanna keep this around
+    stylized.dispose();
+    this.styleButton.textContent = 'Stylize';
   }
 
 }
