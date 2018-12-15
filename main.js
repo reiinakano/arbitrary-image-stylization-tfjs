b9e984fa021b424bc86cc80b713b820b9155caa3
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2018-12-15 20:11:07 +0900

    benchmark code (#12)
    * benchmark code
    
    * add mem profiling

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ class Main {
       this.loadMobileNetStyleModel(),
       this.loadSeparableTransformerModel(),
     ]).then(([styleNet, transformNet]) => {
-      console.log('Loaded styleNet');  
+      console.log('Loaded styleNet');
       this.styleNet = styleNet;
       this.transformNet = transformNet;
       this.enableStylizeButtons()
@@ -413,6 +413,69 @@ class Main {
     stylized.dispose();
   }
 
+  async benchmark() {
+    const x = tf.randomNormal([1, 256, 256, 3]);
+    const bottleneck = tf.randomNormal([1, 1, 1, 100]);
+
+    let styleNet = await this.loadInceptionStyleModel();
+    let time = await this.benchmarkStyle(x, styleNet);
+    styleNet.dispose();
+
+    styleNet = await this.loadMobileNetStyleModel();
+    time = await this.benchmarkStyle(x, styleNet);
+    styleNet.dispose();
+
+    let transformNet = await this.loadOriginalTransformerModel();
+    time = await this.benchmarkTransform(
+        x, bottleneck, transformNet);
+    transformNet.dispose();
+
+    transformNet = await this.loadSeparableTransformerModel();
+    time = await this.benchmarkTransform(
+      x, bottleneck, transformNet);
+    transformNet.dispose();
+
+    x.dispose();
+    bottleneck.dispose();
+  }
+
+  async benchmarkStyle(x, styleNet) {
+    const profile = await tf.profile(() => {
+      tf.tidy(() => {
+        const dummyOut = styleNet.predict(x);
+        dummyOut.print();
+      });
+    });
+    console.log(profile);
+    const time = await tf.time(() => {
+      tf.tidy(() => {
+        for (let i = 0; i < 10; i++) {
+          const y = styleNet.predict(x);
+          y.print();
+        }
+      })
+    });
+    console.log(time);
+  }
+
+  async benchmarkTransform(x, bottleneck, transformNet) {
+    const profile = await tf.profile(() => {
+      tf.tidy(() => {
+        const dummyOut = transformNet.predict([x, bottleneck]);
+        dummyOut.print();
+      });
+    });
+    console.log(profile);
+    const time = await tf.time(() => {
+      tf.tidy(() => {
+        for (let i = 0; i < 10; i++) {
+          const y = transformNet.predict([x, bottleneck]);
+          y.print();
+        }
+      })
+    });
+    console.log(time);
+  }
 }
 
 function getRndInteger(min, max) {
